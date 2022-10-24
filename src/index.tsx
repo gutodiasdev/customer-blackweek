efd2b23770a63ac28a21d07579deeec669466213
Augusto Dias <gutodiasux@gmail.com>
2022-10-24 15:00:31 -0300

    feat: add react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
 import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} />
+  )
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,5 @@
+export function LadingPage () {
+  return (
+    <h1>Landing page!</h1>
+  );
+}

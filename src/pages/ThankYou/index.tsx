769b4edf3c2c6ebe586657a8a6407d68487a7967
Augusto Dias <gutodiasux@gmail.com>
2022-10-26 10:52:01 -0300

    fix: alignments

--- src/pages/ThankYou/index.tsx
+++ src/pages/ThankYou/index.tsx
@@ -13,7 +13,7 @@ export function ThankYou () {
       <Helmet>
         <title>Muito Obrigado - Suzano Black Week</title>
       </Helmet>
-      <main className='flex flex-col justify-center gap-24 px-4 lg:px-40 bg-black h-screen w-full'>
+      <main className='flex flex-col justify-center gap-24 px-4 lg:px-72 bg-black h-screen w-full'>
         <button
           className='text-slate-100 border-[1px] border-slate-100 px-4 py-2 rounded-md mt-8 w-[25%] lg:w-[5%]'
           onClick={handleBackToHome}
@@ -25,12 +25,12 @@ export function ThankYou () {
           <p className='text-[1rem]'>Em breve você receberá um contato</p>
         </div>
         <div className='flex flex-col justify-center gap-16 bg-black h-screen w-full'>
-          <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[45%] font-bold text-black xl:text-4xl xl:mt-8'>
+          <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[60%] font-bold text-black xl:text-4xl xl:mt-8'>
             <a href="https://loja.suzano.com.br/suzano/pt/login?utm_source=google_ads&utm_medium=lp_black_friday&utm_campaign=ja_sou_cliente_suzano" target='_blank' rel="noreferrer">
               JÁ SOU CLIENTE, QUERO IR ÀS COMPRAS
             </a>
           </button>
-          <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[45%] font-bold text-black xl:text-4xl xl:mt-8'>
+          <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[60%] font-bold text-black xl:text-4xl xl:mt-8'>
             <a href="https://loja.suzano.com.br/customer/form?utm_source=google_ads&utm_medium=lp_black_friday&utm_campaign=cadastro_novo_clientes " target='_blank' rel="noreferrer">
               QUERO ME CADASTRAR NO ECOMMERCE!
             </a>

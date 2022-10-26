8815ed34ca26d046bab40c2fc70f83edc5e236b9
Augusto Dias <gutodiasux@gmail.com>
2022-10-26 10:48:39 -0300

    feat: add buttons at ty page

--- src/pages/ThankYou/index.tsx
+++ src/pages/ThankYou/index.tsx
@@ -24,11 +24,18 @@ export function ThankYou () {
           <h2 className='text-[3rem] font-semibold'>Muito obrigado!</h2>
           <p className='text-[1rem]'>Em breve você receberá um contato</p>
         </div>
-        <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[45%] font-bold text-black xl:text-4xl xl:mt-8'>
-          <a href="https://loja.suzano.com.br/" target='_blank' rel="noreferrer">
-            QUERO ME CADASTRAR NO ECOMMERCE!
-          </a>
-        </button>
+        <div className='flex flex-col justify-center gap-16 bg-black h-screen w-full'>
+          <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[45%] font-bold text-black xl:text-4xl xl:mt-8'>
+            <a href="https://loja.suzano.com.br/suzano/pt/login?utm_source=google_ads&utm_medium=lp_black_friday&utm_campaign=ja_sou_cliente_suzano" target='_blank' rel="noreferrer">
+              JÁ SOU CLIENTE, QUERO IR ÀS COMPRAS
+            </a>
+          </button>
+          <button className='bg-[#FFF002] p-4 lg:p-4 rounded-md lg:w-[45%] font-bold text-black xl:text-4xl xl:mt-8'>
+            <a href="https://loja.suzano.com.br/customer/form?utm_source=google_ads&utm_medium=lp_black_friday&utm_campaign=cadastro_novo_clientes " target='_blank' rel="noreferrer">
+              QUERO ME CADASTRAR NO ECOMMERCE!
+            </a>
+          </button>
+        </div>
       </main>
     </>
   );
